9e5977a037d23829b5a501a3ea447dd1c40648fd
Thiago-Mateo <102254270+Thiago-Mateo@users.noreply.github.com>
2022-08-17 16:29:11 -0300

    Update Ej7.c

--- In & Out/Ej7.c
+++ In & Out/Ej7.c
@@ -1,9 +1,26 @@
-//Sin completar
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(void) {
 
-
+  srand(10);
+  int rand_amount;
+  int min;
+  int max;
+  scanf("%d", &rand_amount);
+  int i = 0;
+  while(i < 1){
+    scanf("%d", &min);
+    scanf("%d", &max);
+    i++;
+  }
+  while(rand_amount > 0){
+    int rand_numb = (rand() % max-min) + min;
+    printf("%d ", rand_numb);
+    rand_amount--;
+  }
+  
+  
+  
   return 0;
 }
