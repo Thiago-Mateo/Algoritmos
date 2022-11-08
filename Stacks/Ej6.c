a849c00b29652323c238cc81339d7eda314ad87d
Thiago Mateo <Tmateo2005@hotmail.com>
2022-11-08 12:10:08 -0300

    AED 8/11

---
+++ Stacks/Ej6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int n = 0;
+int size = 1;
+int *stack;
+
+void push(int item){
+  if (n == size) stack = realloc(stack, (size*=2)*sizeof(int));
+  stack[n++] = item;
+}
+
+bool isempty(void){
+  return(n == 0);
+}
+
+int pop(void){
+  int item = stack[--n];
+  stack[n] = 0;
+  if (n > 0 && n == size/4) stack = realloc(stack, (size/=2)*sizeof(int));
+  return item;
+}
+
+int main(void){
+
+}
